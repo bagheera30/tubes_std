14d632baec8a98a8d4ad81de741c22c103e03c20
bagheera30 <rizkialfian30103@gmail.com>
2023-01-03 21:42:37 +0700

    pasien

--- main.cpp
+++ main.cpp
@@ -4,14 +4,47 @@ using namespace std;
 
 int main()
 {
-    ListDOK D;
-    infotypeDOK infoDOK;
-    adrDK K;
-    int n;
+    ListDokter LD;
+    ListPAS PA;
+    infotypeDOK info;
+    adr_dokter K;
+    adrPAS ps;
+    dokter d;
+    creatListDokter(LD);
+    createLP(PA);
+
     pilih=pilihmenu();
     while(pilih!=0){
         if(pilih==1){
+            if(d!=NULL){
+                tambahps(PA,LD,spealisasi,tanggal);
+            }else{
+                cout<<"dokter harus diisi terlebih dahulu"<<endl
+            }
             tambahps(PA,LD,speasialisasi,tanggal);
+        }else if(pilih==2){
+			cout << "=== Tambah Data ===" << endl;
+			cout << "ID : ";
+			cin >> d.id;
+			cout << "Nama : ";
+			cin >> d.nama;
+			cout << "Spesialisasi : ";
+			cin >> d.spesialisasi;
+			cout << "Tanggal (dd-mm-yyyy) : ";
+			cin >> d.tanggal;
+			cout << "Jam Praktek (Start) : ";
+			cin >> d.jam_praktek_start;
+			cout << "Jam Praktek (End) : ";
+			cin >> d.jam_praktek_end;
+			cout << "Kuota Pasien : ";
+			cin >> d.kuota;
+			adr_dokter newdata = createElementDokter(d);
+			insertFirstDokter(LD,newdata);
+        }else if(pilih==3){
+            showPAS(PA);
+        }else if(pilih==4){
+            cout<<"-----data sebelum pindah---------"<<endl;
+            cout<<""
         }
     }
 
--- tubes.cpp
+++ tubes.cpp
@@ -84,127 +84,96 @@ void deletePS(ListPAS &PA,string mid,string namaPS){
 
 	}
 }
-adrPAS penyakit(ListPAS &PA,string hasil){
-
+void biaya(ListPAS &PA,ListDokter &LD,string speasialis,string tanggal,string nama,string mid){
+     adrPAS ps=findps(PA,nama,mid);
+     adr_dokter data=findDokter(LD,speasialis,tanggal);
+     if(ps!=NULL){
+        if()
+     }
 }
-void creatListDokter(ListDokter &LD){
-	(LD).first = NULL;
-}
-adr_dokter createElementDokter(dokter data){
-	adr_dokter a = new element_dokter;
-	(a)->next = NULL;
-	(a)->info = data;
-	return a;
-}
+void showtanggal(ListDokter &LD,ListPAS &PA,string tanggal){
+    adr_dokter data=first(LD);
+    adrPAS ps;
+    int jum=0
+    while(data!=NULL){
+        dokter r=info(data);
+        if(r.tanggal==tanggal){
+            jum++;
+            showPAS(PA);
+        }
+        data=next(data);
 
-void insertFirstDokter(ListDokter &LD,adr_dokter data){
-	if((LD).first != NULL){
-		(data)->next = (LD).first;
-		(LD).first = data;
-	}else{
-		(LD).first = data;
-	}
-}
-
-void showDokter(ListDokter LD){
-	adr_dokter q = (LD).first;
-	if(q!=NULL){
-		cout << "=== Data Dokter ===" << endl;
-		while(q != NULL){
-			dokter d = (q)->info;
-			cout << "ID : " <<  d.id << endl;
-			cout << "Nama : " <<  d.nama << endl;
-			cout << "Spesialisasi : " <<  d.spesialisasi << endl;
-			cout << "Tanggal (dd-mm-yyyy) : " <<  d.tanggal << endl;
-			cout << "Jam Praktek (Start) : " <<  d.jam_praktek_start  << endl;
-			cout << "Jam Praktek (End) : " <<  d.jam_praktek_end  << endl;
-			cout << "Kuota Pasien : " <<  d.kuota  << endl << endl;
-			q = (q)->next;
-		}
-		cout << endl;
-	}else {
-		cout << "Data Dokter Kosong :( " << endl;
-	}
-}
-
-
-bool checkDokter(ListDokter LD,string spesialisasi,string tanggal){
-	adr_dokter q = (LD).first;
-	bool ans=false;
-	if(q!=NULL){
-		while(q != NULL){
-			dokter d = (q)->info;
-			if (d.tanggal == tanggal && d.spesialisasi == spesialisasi) {
-				ans=true;
-			}
-			q = (q)->next;
-		}
-	}
-	return ans;
-}
-
-void deleteDokter(ListDokter &LD, string nama,string tanggal){
-	adr_dokter q = (LD).first;
-	if(q!=NULL){
-		bool flag = false;
-		adr_dokter qq = q;
-		while(q != NULL){
-			dokter d = (q)->info;
-			if (d.tanggal == tanggal && d.nama == nama) {
-				flag = true;
-				break;
-			}
-			qq=q;
-			q = (q)->next;
-		}
-
-		if (flag) {
-			if(q == (LD).first){
-				adr_dokter p = (LD).first;
-				(LD).first = (p)->next;
-				(p)->next = NULL;
-			}else if ((q)->next == NULL) {
-				(qq)->next = NULL;
-			}else{
-				(qq)->next = (q)->next;
-				(q)->next = NULL;
-			}
-			cout << "Dokter berhasil di hapus" << endl;
-		}else {
-			cout << "Dokter dengan nama " << nama << " tidak tersedia pada tanggal " << tanggal << endl;
-		}
-
-	}
-}
-adr_dokter findDokter(ListDokter LD,string spesialisasi,string tanggal){
-	adr_dokter q = (LD).first;
-	adr_dokter ans;
-	if(q!=NULL){
-
-		while(q != NULL){
-			dokter d = (q)->info;
-			if (d.tanggal == tanggal && d.spesialisasi == spesialisasi) {
-				ans=q;
-			}
-			q = (q)->next;
-		}
-	}
-	return ans;
-}
-void tambahps(ListPAS &PA,ListDokter &LD,string spesialisasi,string tanggal){
-    data=findDokter(LD,spesialisasi,tanggal);
-    if(info(data).kouta!=0){
-        dataBaruPas(PA,ps);
-        jadwalD(ps)=data;
-        info(data).kouta--
     }
 }
+void tambahps(ListPAS &PA,ListDokter &LD,string spesialisasi,string tanggal){
+    adr_dokter data=findDokter(LD,spesialisasi,tanggal);
+    adrPAS ps;
+    if(data!=NULL){
+        if(info(data).kouta>0){
+            pas p=info(ps)
+            cout<<"medical record Id: ";cin>>p.mID;
+            cout<<"nama : ";cin>>p.namaPS;
+            cout<<"jenis : ";cin>>p.jenis;
+            cout<<"hasil periksa : ";cin>>p.hasil;
+            cout<<"biaya jasa dokter : ";cin>>p.hasil;
+            cout<<"biaya obat : ";cin>>p.bOB
+
+            ;
+
+            dataBaruPas(PA,ps);
+            jadwalD(ps)=data;
+            info(data).kouta--;
+        }else{
+            cout<<"mohon masukin ke jadwal lain"<<endl;
+        }
+    }else{
+        cout<<"data yang dimasukan salah"<<endl;
+    }
+
+}
+void pindah_jadwal(ListDokter &LD,ListPAS &PA,string namaps,string mid,string spesialisasi_awal,string tanggal_awal,string spesialisasi_akhir,string tanggal_akhir){
+    adr_dokter data1=findDokter(LD,spesialisasi_awal,tanggal_awal);
+    adrPAS ps=findps(PA,namaps,mid)
+    if(data1!=NULL){
+            if(ps!=NULL){
+                if(jadwalD(ps)==data1){
+                    jadwalD(ps)=NULL;
+                }
+                adr_dokter data2=findDokter(LD,spesialisasi_akhir,tanggal_akhir);
+                jadwalD(ps)=data2;
+                ps=next(ps);
+            }else{
+                cout<<"pasien yang dicari tidak ada"<<endl;
+            }
+
+        }else{
+            cout<<"dokter yang di cari tidak ada"<<endl;
+    }
+}
+adrPAS findps(ListPAS &PA,string namaps,string mid){
+    adrPAS P=first(PA);
+    adrPAS h;
+    while(P!=NULL){
+        pas ps=info(P);
+        if(ps.namaPS==namaps&&ps.mID==mid){
+            h=P;
+        }else{
+            h=NULL;
+        }
+        P=next(P);
+    }
+    return h
+}
+
+
 int pilihmenu(){
     int input;
     cout<<"========== Rumah Sakit  ========="<<endl;
     cout<<"1. tambah pasien"<<endl;
     cout<<"2. tambah dokter"<<endl;
     cout<<"3. tampilkan data pasien "<<endl;
+    cout<<"4. pindah jadwal "<<endl;
+    cout<<"5. total biaya "<<endl;
     cout<<"0.keluar "<<endl;
     cout<<"pilih menu:"
     cin>>input;
